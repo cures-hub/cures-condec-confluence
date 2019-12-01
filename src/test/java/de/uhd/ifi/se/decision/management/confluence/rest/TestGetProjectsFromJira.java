93dba8d23d68bb77246e35f27c39ac2f1c9cfe9e
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-12-01 20:53:40 +0100

    Add test class for getProjectsFromJira method

---
+++ src/test/java/de/uhd/ifi/se/decision/management/confluence/rest/TestGetProjectsFromJira.java
@@ -0,0 +1,30 @@
+package de.uhd.ifi.se.decision.management.confluence.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.atlassian.sal.api.component.ComponentLocator;
+import com.sun.jersey.api.client.ClientResponse.Status;
+
+import de.uhd.ifi.se.decision.management.confluence.mocks.MockComponentLocator;
+import de.uhd.ifi.se.decision.management.confluence.rest.impl.KnowledgeRestImpl;
+
+public class TestGetProjectsFromJira {
+	private KnowledgeRest knowledgeRest;
+
+	@Before
+	public void setUp() {
+		ComponentLocator.setComponentLocator(new MockComponentLocator());
+		knowledgeRest = new KnowledgeRestImpl();
+	}
+
+	@Test
+	public void testInvalid() {
+		Response response = knowledgeRest.getProjectsFromJira();
+		assertEquals(Status.OK.getStatusCode(), response.getStatus());
+	}
+}
